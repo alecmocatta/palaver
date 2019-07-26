e8fdeaf9c928177dd1b89f0e7554d1a1545fc85b
alecmocatta <alec@mocatta.net>
2019-07-26 10:30:59 +0100

    v0.2.4

--- src/lib.rs
+++ src/lib.rs
@@ -6,7 +6,7 @@
 //!
 //! `palaver` = "Platform Abstraction Layer" + pa·lav·er *n.* prolonged and tedious fuss.
 
-#![doc(html_root_url = "https://docs.rs/palaver/0.2.3")]
+#![doc(html_root_url = "https://docs.rs/palaver/0.2.4")]
 #![warn(
 	missing_copy_implementations,
 	missing_debug_implementations,
